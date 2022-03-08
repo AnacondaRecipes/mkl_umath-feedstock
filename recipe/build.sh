e357f3d2aa94ddaf6f16e37b2f57ddfe7ba1010d
Anthony DiPietro <51377278+adipietro18@users.noreply.github.com>
2022-03-08 14:28:25 -0500

    Include sysroot before other paths (#2)

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 export MKLROOT=${PREFIX}
-export LDFLAGS="-L${BUILD_PREFIX}/lib -L${PREFIX}/lib ${LDFLAGS}"
-export CFLAGS="-I${BUILD_PREFIX}/include -I${PREFIX}/include ${CFLAGS}"
+export LDFLAGS="-L${BUILD_PREFIX}/${HOST}/sysroot/usr/lib -L${BUILD_PREFIX}/lib -L${PREFIX}/lib ${LDFLAGS}"
+export CFLAGS="-I${BUILD_PREFIX}/${HOST}/sysroot/usr/include -I${BUILD_PREFIX}/include -I${PREFIX}/include ${CFLAGS}"
 
 export CC=icx
 export CXX=icpx
